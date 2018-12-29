7d16de098e7faf6f6accf1ebd8bf41a1df9a1af4
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 07:29:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,21 @@
- const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', function(msg) {
-    const prefix = 'البرفيكس'
-    if(msg.content.startsWith (prefix  + '528318975541641246')) {
-      let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setThumbnail(msg.guild.iconURL)
-      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
-      .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
-      .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
-      .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
-      .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
-      .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
-      .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
-      .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
-      .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
-      .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
-      msg.channel.send({embed:embed});
-    }
-  });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hi') {
+    msg.reply('Hello');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'اشلونك') {
+    msg.reply('بخير وانته');
+  }
+});
+
 
 
 client.login('NTI4NDE0MzU3ODczMzYwODk2.DwiC5w.b32z4Sj62WRB_WXsVJEjok6mvuU');
