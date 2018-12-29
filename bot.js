76d351fe254e4cef675e35de9c50e872b5820e5e
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 07:49:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const Eris = require("eris");
-var x5bzteam = new Eris("NTI3NjU3NDY2NTc4NzMxMDA4.DwgWvw.T9cnCrO1gWf9A3q-WJZtOI1OPKI");
-x5bzteam.on("ready", () => {
-    console.log("Yo! Changer [ON]");
-    onstart();
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '528318975541641250').setName("W");
+client.channels.find('id', '528318975541641250').setName("We");
+client.channels.find('id', '528318975541641250').setName("Wel");
+client.channels.find('id', '528318975541641250').setName("Welc");
+client.channels.find('id', '528318975541641250').setName("Welco");
+client.channels.find('id', '528318975541641250').setName("Welcom");
+client.channels.find('id', '528318975541641250').setName("Welcome");
+client.channels.find('id', '528318975541641250').setName("Welcome T");
+client.channels.find('id', '528318975541641250').setName("Welcome To");
+client.channels.find('id', '528318975541641250').setName("Welcome To T");
+client.channels.find('id', '528318975541641250').setName("Welcome To ox");
+client.channels.find('id', '528318975541641250').setName("Welcome To Tox");
+client.channels.find('id', '528318975541641250').setName("Welcome To Toxic");
+client.channels.find('id', '528318975541641250').setName("Welcome To Toxic C");
+client.channels.find('id', '528318975541641250').setName("Welcome To Toxic Co");
+client.channels.find('id', '528318975541641250').setName("Welcome To Toxic Cod");
+client.channels.find('id', '528318975541641250').setName("Welcome To Toxic Code");
+client.channels.find('id', '528318975541641250').setName("Welcome To Toxic Codes");
+  }, 4000);
 });
-x5bz = "528318975541641246";
-nick = "IRAQMAXYT";
-function onstart(){//Toxic Codes
-    var v1 = nick.split('');//Toxic Codes
-    var counter;
-    var i=0;
-var x5bz2 = ' ';
-  var x5bz3 =   setInterval(function(){//Toxic Codes
-     
-     if (i == v1.length){//Toxic Codes
-     clearInterval(x5bz3);//Toxic Codes
-     onstart()
-      return;
-     }
-     var v2 = v1[i];
-    x5bz2 += v2;
-    x5bzteam.editNickname(x5bz, x5bz2);//Toxic Codes
- 
-        i++;
-   
-     }, 1000);//Toxic Codes
-}
-x5bzteam.connect();//Toxic Codes
-
 client.login('NTI3NjU3NDY2NTc4NzMxMDA4.DwgWvw.T9cnCrO1gWf9A3q-WJZtOI1OPKI');
