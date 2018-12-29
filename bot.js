70f633cebe8ed221a336f64bfe7197a67ce0f815
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-29 07:40:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,32 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const Eris = require("eris");
+var x5bzteam = new Eris("NTI3NjU3NDY2NTc4NzMxMDA4.DwgWvw.T9cnCrO1gWf9A3q-WJZtOI1OPKI");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
 });
-
-client.on('message', msg => {
-  if (msg.content === 'Hi') {
-    msg.reply('Hello');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === 'اشلونك') {
-    msg.reply('بخير وانته');
-  }
-});
-
-
+x5bz = "528318975541641246";
+nick = "IRAQMAXYT";
+function onstart(){//Toxic Codes
+    var v1 = nick.split('');//Toxic Codes
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){//Toxic Codes
+     
+     if (i == v1.length){//Toxic Codes
+     clearInterval(x5bz3);//Toxic Codes
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);//Toxic Codes
+ 
+        i++;
+   
+     }, 1000);//Toxic Codes
+}
+x5bzteam.connect();//Toxic Codes
 
 client.login('NTI4NDE0MzU3ODczMzYwODk2.DwiC5w.b32z4Sj62WRB_WXsVJEjok6mvuU');
